634089f9eedd659c68664b0ccf0b4d2869ec52f1
NipunLiyanage69 <nipun.rightmo@gmail.com>
2024-06-27 21:40:21 +0530

    homepage with alpine fetch

---
+++ README.md
@@ -0,0 +1,17 @@
+# Alpine Store
+
+This is a simple web page that demonstrates an e-commerce store using [Alpine.js](https://alpinejs.dev/)
+
+## Usage
+
+```bash
+bun install
+```
+
+```bash
+bun run dev
+```
+
+```bash
+bunx tailwindcss -i ./style.css -o ./style_build.css --watch
+```
--- main.js
+++ main.js
@@ -7,6 +7,31 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 
+// Alpine.data('cards', () => ({
+//     cards:[],
+//     init() {
+//         fetch('https://fakestoreapi.com/products')
+//         .then(response => response.json())
+//         .then(json => this.cards = json)
+//         // this.cards = {
+//         //     cards: [
+//         //         {
+//         //             name: 'Milk',
+//         //             description: 'Cow\'s milk with butter'
+//         //         },
+//         //         {
+//         //             name: 'Eggs',
+//         //             description: 'Eggs with bacon and cheese'
+//         //         },
+//         //         {
+//         //             name: 'Bread',
+//         //             description: 'Whole grain bread with milk'
+//         //         }
+//         //     ]
+//         // }
+//     }
+// }))
+
 // document.querySelector('#app').innerHTML = `
 //   <div>
 //     <a href="https://vitejs.dev" target="_blank">
